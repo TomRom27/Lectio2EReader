5be14c7c37bfc4b56a0b0967f6cbfd7725f62b9c
Tomek Romanowski <tomek.romanowski@gmail.com>
2018-07-31 21:37:55 +0200

    Pubisging profile added

--- Lectio2EReader/Lectio2EReader/Main.cs
+++ Lectio2EReader/Lectio2EReader/Main.cs
@@ -11,6 +11,7 @@ namespace Lectio2EReader
     public static class Main
     {
         [FunctionName("SendToEReader")]
+        [Disable("IsFuncDisabled")]
         public static async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, TraceWriter log, ExecutionContext context)
         {
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
@@ -27,7 +28,7 @@ namespace Lectio2EReader
             var sender = new EmailSender(config);
             try
             {
-                await sender.Send("tomek.romanowski@gmail.com", "tomekr.kindle@or.pl", "TEST 1", "");
+                await sender.Send("tomek.romanowski@gmail.com", "tomekr.kindle@or.pl", "TEST 1", "Sent at "+DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
 
             }
             catch (Exception ex)
